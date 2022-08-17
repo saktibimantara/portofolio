39762805a1e4f07ea56936c8d4de981e3ce99703
Ragil Bimantara <ryobimantara@gmail.com>
2022-08-17 12:06:49 +0000

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-5349a7c2480b65a31353.js"
+    "url": "webpack-runtime-ef4f1ad2c2bb08bb5fb9.js"
   },
   {
     "url": "framework-aa35e29fbb74d6100667.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "50214be148f030c714cecd7836d566a6"
+    "revision": "bae348cd5df72bd691368d5b41cd10ec"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-50fcc746a3b9bd6c1224.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "348d3eeaf8cb951bf186f6b565e99d7b"
+    "revision": "270b63e315d9edb9d28e970eb687221b"
   },
   {
     "url": "polyfill-f4e05b864c9bf93fe2ca.js"
